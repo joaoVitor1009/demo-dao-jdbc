a024e6ca9a256a024cf1c87c0dbf1e20261efe33
joaoVitor1009 <joaovitorsg1804@gmail.com>
2026-03-13 14:57:55 -0300

    Criada Classe Seller e Funcional

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -1,6 +1,9 @@
 package Application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
@@ -8,6 +11,7 @@ public class Program {
 		
 		Department obj = new Department(1, "Books");
 		
+		Seller seller = new Seller(21, "bob", "bob@gmail.com", new Date(), 3000.0, obj);
 	}
 
 }
